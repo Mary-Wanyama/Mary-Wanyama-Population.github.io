5309a63e2d4d9dd463ff2c37e2a7de10500ac8a7
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-07 01:56:18 +0300

    This commit adds an even t listener to the first form as well as adding a function to prevent it from refreshing automatically

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const express = require('express')
-const app = require('app')
-app.listen(3000, function() {
-    console.log('success')
-})
+
+ let url
+ let urlPath;
+document.querySelector('#form1').addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    // const userName = document.querySelector('#text1').value
+
+    // const formData = new FormData()
+    // formData.append('username',userName)
+
+    // url = 'https://api.genderize.io/'
+    // urlPath= `${url}?name=${userName}`
+
+    // fetch(urlPath, {
+    //     method : 'POST',
+    //     Body : formData
+    // })
+    // .then(res =>res.json())
+    // .then(data => console.log(data))
+    // .catch(error => console.log(error))
+
+    // document.querySelector('#name').innerHTML = urlPath.name
+    // document.querySelector('#gender1').innerHTML = urlPath.gender
+    // document.querySelector('#prob').innerHTML = urlPath.probability
+    // document.querySelector('#count').innerHTML = urlPath.count
+} )
+
+
