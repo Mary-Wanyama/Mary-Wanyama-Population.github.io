c9bf7989c697030464f74e789c13ccd37f56fae3
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-07 01:59:19 +0300

    This commit adds a fetch request. This allows the user to access the analysis done on the text entered in the form which is then posted on the DOM

--- script.js
+++ script.js
@@ -4,21 +4,21 @@
 document.querySelector('#form1').addEventListener('submit', function(e) {
     e.preventDefault()
 
-    // const userName = document.querySelector('#text1').value
+    const userName = document.querySelector('#text1').value
 
-    // const formData = new FormData()
-    // formData.append('username',userName)
+    const formData = new FormData()
+    formData.append('username',userName)
 
-    // url = 'https://api.genderize.io/'
-    // urlPath= `${url}?name=${userName}`
+    url = 'https://api.genderize.io/'
+    urlPath= `${url}?name=${userName}`
 
-    // fetch(urlPath, {
-    //     method : 'POST',
-    //     Body : formData
-    // })
-    // .then(res =>res.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.log(error))
+    fetch(urlPath, {
+        method : 'POST',
+        Body : formData
+    })
+    .then(res =>res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
 
     // document.querySelector('#name').innerHTML = urlPath.name
     // document.querySelector('#gender1').innerHTML = urlPath.gender
